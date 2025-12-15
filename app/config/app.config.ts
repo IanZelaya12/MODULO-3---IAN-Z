fdf26d43a8be2243279bde54f3c03313a5518977
IanZelaya12 <ianzela0435@gmail.com>
2025-12-14 21:13:17 -0600

    Create app.config.ts

---
+++ app/config/app.config.ts
@@ -0,0 +1,5 @@
+// Configuración de la URL de Ngrok (debe coincidir con tu túnel Ngrok)
+export const AppConfig = {
+    // 3. Variable de configuración para la URL de Ngrok
+    NGROK_URL: 'TU_URL_DE_NGROK', // Ejemplo: 'https://a1b2-3c4d-5e6f-7g8h.ngrok-free.app'
+};
